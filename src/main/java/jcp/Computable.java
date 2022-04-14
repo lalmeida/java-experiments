4e1e12fd0e5ec528520301d678b0370342af463c
lalmeida <leonardo.fig.almeida@gmail.com>
2022-04-13 22:54:02 -0300

    My solution to the last problem presented in the Part 1 of the book Java Concurrency in Practice (by Brian Goetz and others)
     It is basically a simplified concurrent cache with no expiration policy or size limits.

---
+++ src/main/java/jcp/Computable.java
@@ -0,0 +1,7 @@
+package jcp;
+
+public interface Computable<A, V> {
+
+     V compute(A arg) throws InterruptedException;
+
+}
---
+++ src/test/java/jcp/CachedComputableTest.java
@@ -0,0 +1,47 @@
+package jcp;
+
+import jcp.CachedComputable;
+import jcp.Computable;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CachedComputableTest {
+
+    private ComputableStub computableStub = new ComputableStub();
+
+    CachedComputable cachedComputable = new CachedComputable<String, String>(computableStub);
+
+    @Test
+    void delegates_to_Computable() throws InterruptedException {
+        assertEquals("B1", cachedComputable.compute("A1"));
+    }
+
+    @Test
+    @Timeout(1) // the computation is cached, hence several calls should still take less than a second
+    void same_request_several_times_should_hit_compute_service_only_once_and_be_fast() throws InterruptedException {
+        cachedComputable.compute("A1");
+        cachedComputable.compute("A1");
+        cachedComputable.compute("A1");
+        cachedComputable.compute("A1");
+        assertEquals(1 , computableStub.serviceCalls);
+    }
+
+    public CachedComputable getCachedComputable() {
+        return cachedComputable;
+    }
+
+    private static class ComputableStub implements Computable<String, String> {
+        int serviceCalls = 0;
+
+        @Override
+        public String compute(String arg) throws InterruptedException {
+            serviceCalls++;
+            Thread.sleep(700); //emulate long service processing
+            if (arg.equals("A1")) return "B1";
+
+            return null;
+        }
+    }
+}

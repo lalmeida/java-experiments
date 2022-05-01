bb3f9aa2e7bbb9dd3367adf1aaa501e4e53178e8
lalmeida <leonardo.fig.almeida@gmail.com>
2022-05-01 11:27:23 -0300

    Ovreall Refactoring - extracting and renaming lots of methods

--- src/main/java/site/tddmanifesto/stringcalculator/StringCalculator.java
+++ src/main/java/site/tddmanifesto/stringcalculator/StringCalculator.java
@@ -2,31 +2,49 @@ package site.tddmanifesto.stringcalculator;
 
 public class StringCalculator {
 
-    public int add(String s) {
-        String[] parts = s.split("\n");
-        String delimiter = parts[0].substring(2);
-        return processNumbers(parts[1], delimiter);
+    public int add(String inputString) {
+        String[] parts = inputString.split("\n");
+        String delimiter = getDelimiter(parts[0]);
+        return processNumericPart(parts[1], delimiter);
     }
 
-    private int processNumbers(String numberString, String expectedDelimiter) {
+    private String getDelimiter(String delimiterDefinitionPart) {
+        return delimiterDefinitionPart.substring(2);
+    }
+
+    private int processNumericPart(String numericPart, String expectedDelimiter) {
         int sum = 0;
-        for (int i = 0; i < numberString.length() ; ) {
-            int j = i;
-            while ( j < numberString.length() && Character.isDigit(numberString.charAt(j))) j++;
-            if (j>i) { //if we are processing a number
-                int number = Integer.parseInt(numberString, i, j,10);
+        for (int i = 0; i < numericPart.length() ; ) {
+            if (processingANumber(numericPart, i)) {
+                int endOfNumberIndex = getEndOfNumberIndex(numericPart, i);
+                int number = Integer.parseInt(numericPart, i, endOfNumberIndex,10);
                 sum += number;
-                i = j;
-            } else { //it should be the separator
-                String actualDelimiter = numberString.substring(i,i+expectedDelimiter.length());
-                if (expectedDelimiter.equals( actualDelimiter)) {
-                    i = i + expectedDelimiter.length();
-                } else {
-                    throw new IllegalArgumentException("'"+expectedDelimiter+ "' expected, but '" + actualDelimiter + "' found at position " +i+"." );
-                }
+                i = endOfNumberIndex;
+            } else { //we are processing the delimiter
+                i = processDelimiter(numericPart, expectedDelimiter, i);
             }
         }
         return sum;
+    }
+
+    private boolean processingANumber(String numberString, int i) {
+        return Character.isDigit(numberString.charAt(i));
+    }
+
+    private int getEndOfNumberIndex(String numberString, int i) {
+        int j = i;
+        while ( j < numberString.length() && Character.isDigit(numberString.charAt(j)) ) j++;
+        return j;
+    }
+
+    private int processDelimiter(String numberString, String expectedDelimiter, int i) {
+        String actualDelimiter = numberString.substring(i, i + expectedDelimiter.length());
+        if (expectedDelimiter.equals(actualDelimiter)) {
+            i = i + expectedDelimiter.length();
+        } else {
+            throw new IllegalArgumentException("'"+ expectedDelimiter + "' expected, but '" + actualDelimiter + "' found at position " + i +"." );
+        }
+        return i;
     }
 
 }

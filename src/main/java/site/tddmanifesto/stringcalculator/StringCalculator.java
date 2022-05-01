8d4a328a28a49730b4fd7e5b6150dda767fbd0c4
lalmeida <leonardo.fig.almeida@gmail.com>
2022-05-01 10:59:09 -0300

    New requirements: should be able to use arbitrary strings as separators and should show descriptive error message with position

--- src/main/java/site/tddmanifesto/stringcalculator/StringCalculator.java
+++ src/main/java/site/tddmanifesto/stringcalculator/StringCalculator.java
@@ -9,14 +9,26 @@ public class StringCalculator {
     public int add(String s) {
         String[] parts = s.split("\n");
         String delimiter = parts[0].substring(2);
-        return addNumbers(parts[1], delimiter);
+        return processNumbers(parts[1], delimiter);
     }
 
-    private int addNumbers(String numberString, String delimiters) {
+    private int processNumbers(String numberString, String expectedDelimiter) {
         int sum = 0;
-        StringTokenizer tokenizer = new StringTokenizer(numberString, delimiters, true);
-        while (tokenizer.hasMoreTokens()) {
-            sum += processToken(tokenizer.nextToken(), !tokenizer.hasMoreTokens(), delimiters);
+        for (int i = 0; i < numberString.length() ; ) {
+            int j = i;
+            while ( j < numberString.length() && Character.isDigit(numberString.charAt(j))) j++;
+            if (j>i) { //if we are processing a number
+                int number = Integer.parseInt(numberString, i, j,10);
+                sum += number;
+                i = j;
+            } else { //it should be the separator
+                String actualDelimiter = numberString.substring(i,i+expectedDelimiter.length());
+                if (expectedDelimiter.equals( actualDelimiter)) {
+                    i = i + expectedDelimiter.length();
+                } else {
+                    throw new IllegalArgumentException("'"+expectedDelimiter+ "' expected, but '" + actualDelimiter + "' found at position " +i+"." );
+                }
+            }
         }
         return sum;
     }
--- src/main/java/site/tddmanifesto/stringcalculator/StringCalculatorTest.java
+++ src/main/java/site/tddmanifesto/stringcalculator/StringCalculatorTest.java
@@ -9,14 +9,25 @@ public class StringCalculatorTest {
 
     StringCalculator stringCalculator = new StringCalculator();
 
-    @Test void summing_with_semicolon_as_separator() {
+    @Test void should_sum_with_semicolon_as_separator() {
         assertEquals(4, stringCalculator.add("//;\n1;3"));
     }
 
-    @Test void summing_with_pipe_as_separator() {
+    @Test void should_sum_with_pipe_as_separator() {
         assertEquals(6, stringCalculator.add("//|\n1|2|3"));
     }
 
+    @Test void should_sum_with_string_as_separator() {
+        assertEquals(7, stringCalculator.add("//sep\n2sep5"));
+    }
 
+    @Test void when_using_wrong_separator_then_should_show_error_message_with_error_position() {
+        try {
+            stringCalculator.add("//|\n1|2,3");
+            fail("Should throw exception");
+        } catch (IllegalArgumentException e) {
+            assertEquals("'|' expected, but ',' found at position 3.", e.getMessage());
+        }
+    }
 
 }

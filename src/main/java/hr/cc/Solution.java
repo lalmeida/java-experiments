d5cc65605e5af027413096aa6dae2f352230b163
lalmeida <leonardo.fig.almeida@gmail.com>
2022-08-21 21:38:40 -0300

    Camel Case challenge - all in one class due to constraints in the upload platform

---
+++ src/main/java/hr/cc/Solution.java
@@ -0,0 +1,166 @@
+package hr.cc;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* !!!The solution to the Camel Case challenge must be in one class called Solution!!! */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution sol = new Solution();
+
+        List<Operation> operations = sol.readInput(System.in);
+
+        List<String> outputLines = sol.process(operations);
+
+        printResults(outputLines);
+    }
+
+    private static void printResults(List<String> outputLines) {
+
+        for (String line: outputLines) {
+            System.out.println(line);
+        }
+
+    }
+
+    List<Operation> readInput(InputStream in) {
+
+        Scanner scan = new Scanner(in);
+        List<Operation> result = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
+            String[] tokens = scan.nextLine().split(";");
+            result.add(new Solution.Operation(Action.from(tokens[0]), Type.from(tokens[1]), tokens[2]));
+        }
+        return result;
+    }
+
+    List<String> process(List<Operation> operations) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (Operation operation : operations) {
+            if (operation.getAction() == Action.Split) {
+                result.add(split(operation.getName()));
+            } else {
+                result.add(combine(operation.getType(), operation.getName()));
+            }
+        }
+        return result;
+    }
+
+    private static String combine(Type type, String name) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (i == 0) {
+                if (type == Type.Class) {
+                    sb.append(Character.toUpperCase(c));
+                } else {
+                    sb.append(Character.toLowerCase(c));
+                }
+            } else if (Character.isSpaceChar(c)) {
+                c = name.charAt(++i); // advancing iterator and reading. Assuming there is always a non-space character after the space.
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+
+            if (i == name.length() - 1 && type == Type.Method) { // if last character && is a method
+                sb.append("()");// place parenthesis!
+            }
+        }
+        return sb.toString();
+    }
+
+    private static String split(String name) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < name.length(); i++) {
+            char c = name.charAt(i);
+            if (i == 0) { // first character should always be lower case
+                sb.append(Character.toLowerCase(c));
+            } else if (Character.isUpperCase(c)) { // else if upper case
+                sb.append(' ');
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+
+    static class Operation {
+
+        private final Action action;
+        private final Type type;
+        private final String name;
+
+        public Operation(Action action, Type type, String name) {
+            this.action = action;
+            this.type = type;
+            this.name = name;
+        }
+
+        public Action getAction() {
+            return action;
+        }
+
+        public Type getType() {
+            return type;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    enum Action {
+
+        Split("S"), Combine("C");
+
+        private final String code;
+
+        Action(String code) {
+            this.code = code;
+        }
+
+        public static Action from(final String actionCode) {
+            if (actionCode.equals(Split.code)) {
+                return Split;
+            } else if (actionCode.equals(Combine.code) ){
+                return Combine;
+            } else {
+                throw new IllegalArgumentException("No enum constant for:"+actionCode+".");
+            }
+        }
+    }
+
+
+    enum Type {
+
+        Method("M"), Class("C"), Variable("V");
+
+        private final String code;
+
+        Type(String code) {
+            this.code = code;
+        }
+
+        public static Type from(String typeCode) {
+            if (typeCode.equals(Class.code)) {
+                return Class;
+            } else if (typeCode.equals(Method.code) ){
+                return Method;
+            } else if (typeCode.equals(Variable.code) ){
+                return Variable;
+            } else {
+                throw new IllegalArgumentException("No enum constant for:"+typeCode+".");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/hr/cc/SolutionTest.java
@@ -0,0 +1,94 @@
+package hr.cc;
+
+import hr.cc.Solution.Action;
+import hr.cc.Solution.Operation;
+import hr.cc.Solution.Type;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void processSingleLine() {
+
+        List<String> lines =
+                new Solution().process(List.of( new Operation(
+                        Action.Split, Type.Class, "FooBar")));
+
+        assertEquals(1, lines.size());
+        assertEquals("foo bar",lines.get(0));
+
+    }
+
+    @Test
+    void processThreeSplits() {
+
+        List<String> lines =
+                new Solution().process(List.of(
+                        new Operation(Action.Split, Type.Class, "FooBar"),
+                        new Operation(Action.Split, Type.Method, "fooBar"),
+                        new Operation(Action.Split, Type.Variable, "firstName")
+                ));
+
+        assertEquals(3, lines.size());
+        assertEquals("foo bar",lines.get(0));
+        assertEquals("foo bar",lines.get(1));
+        assertEquals("first name",lines.get(2));
+
+    }
+
+    @Test
+    void processThreeSplitsWithASingleWordEach() {
+
+        List<String> lines =
+                new Solution().process(List.of(
+                        new Operation(Action.Split, Type.Class, "Person"),
+                        new Operation(Action.Split, Type.Method, "execute"),
+                        new Operation(Action.Split, Type.Variable, "name")
+                ));
+
+        assertEquals(3, lines.size());
+        assertEquals("person",lines.get(0));
+        assertEquals("execute",lines.get(1));
+        assertEquals("name",lines.get(2));
+
+    }
+
+    @Test
+    void processThreeCombineWithASingleWordEach() {
+
+        List<String> lines =
+                new Solution().process(List.of(
+                        new Operation(Action.Combine, Type.Class, "Person"),
+                        new Operation(Action.Combine, Type.Method, "Execute"),
+                        new Operation(Action.Combine, Type.Variable, "Name")
+                ));
+
+        assertEquals(3, lines.size());
+        assertEquals("Person",lines.get(0));
+        assertEquals("execute()",lines.get(1));
+        assertEquals("name",lines.get(2));
+
+    }
+
+    @Test
+    void processThreeCombines() {
+
+        List<String> lines =
+                new Solution().process(List.of(
+                        new Operation(Action.Combine, Type.Class, "foo Bar"),
+                        new Operation(Action.Combine, Type.Method, "Foo Bar"),
+                        new Operation(Action.Combine, Type.Variable, "First Name")
+                ));
+
+        assertEquals(3, lines.size());
+        assertEquals("FooBar",lines.get(0));
+        assertEquals("fooBar()",lines.get(1));
+        assertEquals("firstName",lines.get(2));
+
+    }
+
+   }

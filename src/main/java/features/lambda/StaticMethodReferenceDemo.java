775c0ca7ec56038c0c8be37e91133b6794a1fbd8
lalmeida <leonardo.fig.almeida@gmail.com>
2022-10-14 11:58:55 -0300

    Playing with lambdas and method references..

---
+++ src/main/java/features/lambda/StaticMethodReferenceDemo.java
@@ -0,0 +1,38 @@
+package features.lambda;
+
+
+interface StringFunc {
+    String func (String str);
+}
+
+class MyStringOps {
+
+    static String strReverse(String str) {
+        StringBuilder sb = new StringBuilder(str.length());
+        for (int i = str.length() -1 ; i >=0 ; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+}
+
+class StaticMethodReferenceDemo {
+
+    public static void main (String[] args) {
+
+        String s = "Lambdas are powerful.";
+
+        // without Method Reference
+        System.out.println(MyStringOps.strReverse( s ));
+
+        // with Method Reference
+        StringFunc instanceWithFunction = MyStringOps::strReverse;
+        System.out.println( instanceWithFunction.func ( s ) );
+
+        // with Method Reference - Standard functional interfaces
+        java.util.function.Function<String, String> instanceWithFunction2 = MyStringOps::strReverse;
+        System.out.println( instanceWithFunction2.apply ( s ) );
+
+    }
+
+}

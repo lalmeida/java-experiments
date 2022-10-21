995c03b209ab41d50c2e03ece4a075e8886a0672
lalmeida <leonardo.fig.almeida@gmail.com>
2022-10-21 12:40:29 -0300

    Playing with lambdas and method references..

--- src/main/java/features/lambda/methodreference/instance/InstanceMethodReferenceDemo.java
+++ src/main/java/features/lambda/methodreference/instance/InstanceMethodReferenceDemo.java
@@ -1,44 +1,55 @@
 package features.lambda.methodreference.instance;
 
-import java.util.function.Function;
+import java.util.Comparator;
 
-interface StringFunc {
-    String func (MyString str);
+class DomainObject {
+    private String description;
+
+    public DomainObject(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int compareTo (DomainObject obj) {
+        return description.compareTo(obj.getDescription());
+    }
+
 }
 
-class MyString {
+class HashComparator {
 
-    private final String str;
-
-    public MyString(String str) {
-        this.str = str;
+    int subtract(DomainObject domainObj1, DomainObject domainObj2) {
+        return domainObj1.hashCode() - domainObj2.hashCode();
     }
 
-    String reverse() {
-        StringBuilder sb = new StringBuilder(str.length());
-        for (int i = str.length() -1 ; i >=0 ; i--) {
-            sb.append(str.charAt(i));
-        }
-        return sb.toString();
-    }
 }
 
 class InstanceMethodReferenceDemo {
 
     public static void main (String[] args) {
 
-        MyString myString = new MyString("Lambdas are powerful.");
+        HashComparator hashComparator = new HashComparator();
 
-        // without Method Reference
-        System.out.println(myString.reverse());
+        DomainObject d1 = new DomainObject("d1");
+        DomainObject d2 = new DomainObject("d2");
 
-        // with Method Reference
-        StringFunc instanceWithFunction = MyString::reverse;
-        System.out.println( instanceWithFunction.func ( myString ) );
+        // without using Method Reference
+        System.out.println(hashComparator.subtract(d1, d2));
 
-        // with Method Reference - Standard functional interfaces
-        Function<MyString,String> instanceWithFunction2 = MyString::reverse;
-        System.out.println(instanceWithFunction2.apply(myString));
+        // with Instance Method Reference - invoking object is not a parameter of the functional interface
+        Comparator<DomainObject> instanceWithFunction1 = hashComparator::subtract;
+        System.out.println( instanceWithFunction1.compare( d1, d2 ));
+
+        // with Instance Method Reference - using invoking object as the first parameter of the functional interface
+        Comparator<DomainObject>   instanceWithFunction2 = DomainObject::compareTo;
+        System.out.println( instanceWithFunction2.compare( d1, d2 ));
+
+
+
+
 
     }
 
